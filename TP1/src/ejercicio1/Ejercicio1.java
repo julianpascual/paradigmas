c4ad6608e8e917213ba3ba7f8049899fa1856c99
julianPascual <julian.pascual@mulesoft.com>
2016-08-24 23:54:49 -0300

    Second commit

---
+++ TP1/src/ejercicio1/Ejercicio1.java
@@ -0,0 +1,7 @@
+package ejercicio1;
+
+/**
+ * Created by julianpascual on 8/24/16.
+ */
+public class Ejercicio1 {
+}
---
+++ TP1/test/ejercicio1/Ejercicio1Test.java
@@ -0,0 +1,10 @@
+package ejercicio1;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by julianpascual on 8/24/16.
+ */
+public class Ejercicio1Test {
+
+}
