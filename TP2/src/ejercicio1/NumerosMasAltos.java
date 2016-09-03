9d080dd6b6ab95bfb5fc681affce69d7f9d97983
julianPascual <julian.pascual@mulesoft.com>
2016-09-03 19:34:30 -0300

    Ejercicio 1 TP 2

--- TP2/src/ejercicio1/NumerosMasAltos.java
+++ TP2/src/ejercicio1/NumerosMasAltos.java
@@ -7,6 +7,23 @@ public class NumerosMasAltos {
 
     public int[] losMasAltos(int[] numeros){
 
-        return null;
+        int[] masAltos = new int[2];
+        masAltos[0] = 0;
+        masAltos[1] = 0;
+
+        for (int i = 0; i < numeros.length ; i++) {
+
+            if (numeros[i] > masAltos[0]) {
+
+                masAltos[1] = masAltos[0];
+                masAltos[0] = numeros[i];
+
+            } else if (numeros[i] > masAltos[1]) {
+
+                masAltos[1] = numeros[i];
+
+            }
+        }
+        return masAltos;
     }
 }
--- TP2/test/ejercicio1/NumerosMasAltosTest.java
+++ TP2/test/ejercicio1/NumerosMasAltosTest.java
@@ -1,19 +1,45 @@
 package ejercicio1;
 
-import static org.junit.Assert.*;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 /**
  * Created by julianpascual on 8/28/16.
  */
 public class NumerosMasAltosTest {
-    @org.junit.Before
-    public void setUp() throws Exception {
+    private NumerosMasAltos numAltos;
 
+    @Before
+    public void setUp() throws Exception {
+        numAltos = new NumerosMasAltos();
     }
 
-    @org.junit.Test
+    @Test
     public void losMasAltos() throws Exception {
+        int[] numeros = {6,2,8,10};
+        int[] losMasAltos = numAltos.losMasAltos(numeros);
 
+        Assert.assertEquals(10, losMasAltos[0]);
+        Assert.assertEquals(8, losMasAltos[1]);
+    }
+
+    @Test
+    public void losMasAltos2() throws Exception {
+        int[] numeros = {4,1,3,2};
+        int[] losMasAltos = numAltos.losMasAltos(numeros);
+
+        Assert.assertEquals(4, losMasAltos[0]);
+        Assert.assertEquals(3, losMasAltos[1]);
+    }
+
+    @Test
+    public void losMasAltos3() throws Exception {
+        int[] numeros = {7,1,2,9,125,9,12};
+        int[] losMasAltos = numAltos.losMasAltos(numeros);
+
+        Assert.assertEquals(125, losMasAltos[0]);
+        Assert.assertEquals(12, losMasAltos[1]);
     }
 
 }
