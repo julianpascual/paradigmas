a5591077e71125f8eb6328fa1520d92c53752f01
julianpascual <julianpascual96@gmail.com>
2016-11-09 01:08:55 -0300

    Spring project

---
+++ spring-project/domain-module/src/main/java/ar/edu/uai/model/person/Person.java
@@ -0,0 +1,56 @@
+package ar.edu.uai.model.person;
+
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "PERSON")
+@Access(AccessType.FIELD)
+public class Person {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "PERSON_ID", unique = true, nullable = false)
+    private Integer id;
+
+    @Column(name = "NAME", nullable = false)
+    private String name;
+
+    @Column(name = "AGE", nullable = false)
+    private Integer age;
+
+    @OneToOne
+    private Person padre;
+
+    public Person() {
+    }
+
+    public Person(Integer id, String name, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Person getPadre() {
+        return padre;
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName() + " [id=" + id + ", name=" + name + ", age=" + age + "]";
+    }
+}
